4500469b80ab8f35b82ae66a5dd74564537ad9d1
pfdp0 <pfdeplaen@users.noreply.github.com>
2020-12-07 16:04:06 +0100

    Add of PHP + editing Nav & csss

---
+++ about.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Quick repair - About</title>
+		<link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
+		<meta name="keywords" content="">
+		<meta name="description" content="">
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=Edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!--
+
+		<!-- stylesheet css -->
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+		<link rel="stylesheet" href="css/font-awesome.min.css">
+		<link rel="stylesheet" href="css/nivo-lightbox.css">
+		<link rel="stylesheet" href="css/nivo_themes/default/default.css">
+		<link rel="stylesheet" href="css/templatemo-style.css">
+		<link rel="stylesheet" href="css/style.css?v=<?=time();?>">
+
+	</head>
+	<body>
+
+<!-- navigation -->
+<?php include_once('nav.php'); ?>
+
+<!-- about header section -->
+	<div id="about-header">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12 col-sm-12"></div>
+			</div>
+		</div>
+	</div>
+
+<!--features-->
+	<div id="features" class="features text-center">
+			<div class="container">
+
+				<div class="row">
+					<div class="col-md-offset-2 col-md-8 col-sm-12">
+						<h2>We Offer!</h2>
+						<p> Here are the features, we offer in our self-repairing club.</p>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-sm-6 col-md-3">
+						<a href="#"><i class="fa fa-gears"></i></a>
+							<h3>Quick repairation service</h3>
+							<p>details</p>
+					</div>
+					<div class="col-sm-6 col-md-3">
+						<a href="#"><i class="fa fa-user-plus"></i></a>
+							<h3>Become a member</h3>
+							<p>details</p>
+					</div>
+					<div class="col-sm-6 col-md-3">
+						<a href="#"><i class="fa fa-cog"></i></a>
+							<h3>Be your own repairer</h3>
+							<p>details</p>
+					</div>
+					<div class="col-sm-6 col-md-3">
+						<a href="#"><i class="fa fa-cloud"></i></a>
+							<h3>Share knowledge</h3>
+							<p>details</p>
+					</div>
+				</div>
+
+				<div class="row">
+					<div class="col-sm-6 col-md-3">
+						<a href="#"><i class="fa fa-video-camera"></i></a>
+							<h3>Tutorials</h3>
+							<p>details</p>
+					</div>
+					<div class="col-sm-6 col-md-3">
+						<a href="#"><i class="fa fa-money"></i></a>
+							<h3>Earn points</h3>
+							<p>details</p>
+					</div>
+					<div class="col-sm-6 col-md-3">
+						<a href="#"><i class="fa fa-wrench"></i></a>
+							<h3>Less-expensive service</h3>
+							<p>details</p>
+					</div>
+
+					<div class="col-sm-6 col-md-3">
+						<a href="#"><i class="fa fa-group"></i></a>
+							<h3>Friendly environment</h3>
+							<p>details</p>
+					</div>
+				</div>
+			</div>
+	</div>
+<!--services-->
+
+
+<!-- footer section -->
+<footer>
+	<div class="container">
+			<div class="col-md-5 col-sm-4">
+				<img src="images/logo.png" class="img-responsive" alt="logo">
+			</div>
+
+			<div class="col-md-4 col-sm-4 newsletter">
+				<p><i class="fa fa-phone"></i> +32-000-00-00-00 </p>
+				<p><i class="fa fa-envelope-o"></i> contact@quickrepair.com </p>
+				<p><i class="fa fa-globe"></i> www.quickrepair.com</p>
+			</div>
+	</div>
+</footer>
+
+<!-- copyright section -->
+<div class="copyright">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6 col-sm-6">
+				<p>Copyright © 2084 Quick repair</p>
+			</div>
+			<div class="col-md-6 col-sm-6">
+				<ul class="social-icons">
+					<li><a href="#" class="fa fa-facebook"></a></li>
+					<li><a href="#" class="fa fa-twitter"></a></li>
+					<li><a href="#" class="fa fa-dribbble"></a></li>
+					<li><a href="#" class="fa fa-pinterest"></a></li>
+					<li><a href="#" class="fa fa-behance"></a></li>
+					<li><a href="#" class="fa fa-envelope-o"></a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</div>
+
+<!-- javascript js -->
+<script src="js/jquery.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/nivo-lightbox.min.js"></script>
+<script src="js/custom.js"></script>
+
+</body>
+</html>
---
+++ nav.php
@@ -0,0 +1,20 @@
+<div class="container">
+	<div class="navbar navbar-default navbar-static-top" role="navigation">
+		<div class="navbar-header">
+			<button class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+				<span class="icon icon-bar"></span>
+				<span class="icon icon-bar"></span>
+				<span class="icon icon-bar"></span>
+			</button>
+			<a href="#" class="navbar-brand"><img src="images/logo.png" class="img-responsive" alt="logo"></a>
+		</div>
+		<div class="collapse navbar-collapse">
+			<ul class="nav navbar-nav navbar-right">
+				<li><a href="index.php" class="active"><i class="fa fa-home fa-3x"></i></a></li>
+				<li><a href="about.php">A PROPOS</a></li>
+				<li><a href="membership.php">SOUSCRIRE</a></li>
+				<li><a href="contact.php">CONTACT</a></li>
+			</ul>
+		</div>
+	</div>
+</div>
